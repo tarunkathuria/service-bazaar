c0668df5079be90162a4eaea7692c8e36bd684de
Tarun Kathuria <tarunkathuria@gmail.com>
2013-10-15 12:14:28 +0530

    created login and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 ServiceBazaar::Application.routes.draw do
+  get "pages/contact"
+  get "pages/login"
+  post "pages/login"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'pages#login', via: [:get,:post]
+   match "/login", to: "pages#login", via: [:get, :post]
+   match "/contact", to: "pages#contact", via: [:get]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
