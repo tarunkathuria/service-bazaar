0ba0444fb66e9efcebcbd71cd358dd4c743eacdd
Rohan Prinja <rohan.prinja@gmail.com>
2013-11-11 18:08:39 +0530

    Changed DBscript.sql and Exchange.sql so that everything is lowercase

--- public/DBscript.sql
+++ public/DBscript.sql
@@ -4,17 +4,7 @@ create database service_bazaar;
 
 use service_bazaar;
 
-create table User(
-	username varchar(30) primary key,
-	password varchar(20) check (len(password)>=6),
-	latitudeLocation float,
-	longitudeLocation float,
-	realName varchar(60),
-	dateOfBirth date,
-	emailID varchar(40)
-);
-
-create table Buyer(
+create table buyers(
 	username varchar(30) primary key references User(username),
 	password varchar(20) check (len(password)>=6),
 	latitudeLocation float,
@@ -24,7 +14,7 @@ create table Buyer(
 	emailID varchar(40)
 );
 
-create table Vendor(
+create table vendors(
 	username varchar(30) primary key references User(username),
 	password varchar(20) check (len(password)>=6),
 	latitudeLocation float,
@@ -35,7 +25,7 @@ create table Vendor(
 	rating double check(rating >= 0 and rating <=5)
 );
 
-create table BuyerVendor(
+create table buyervendors(
 	username varchar(30) primary key references User(username),
 	password varchar(20) check (len(password)>=6),
 	latitudeLocation float,
@@ -46,94 +36,32 @@ create table BuyerVendor(
 	rating double check(rating >= 0 and rating <=5)
 );
 
-create table Category(
-	categoryID integer primary key auto_increment,
-	Name varchar(40),
-	Description varchar(500)
-);
 
-create table Service(
-	serviceID integer primary key auto_increment,
-	title varchar(100),
-	description varchar(500),
-	visibility integer
-);
 
-create table LiesIn(
+create table liesins(
 	categoryID integer references Category(categoryID),
 	serviceID integer references Service(ServiceID),
 	primary key(serviceID, categoryID)
 );
 
-create table Feedback(
-	feedbackID integer primary key auto_increment,
-	rating double check (rating >= 0 and rating <= 5),
-	review varchar(4000)
-);
 
-create table FeedbackFor(
+create table feedbackfors(
 	feedbackID integer references Feedback(feedbackID),
 	buyerName varchar(30) references Buyer(username),
 	serviceID integer references Service(serviceID),
 	primary key(buyerName, serviceID)
 );
 
-create table Message(
-	messageID integer primary key auto_increment,
-	messageDate date,
-	subject varchar(100),
-	messageText varchar(2000)
-);
 
-create table Exchange(
+create table exchanges(
 	senderName varchar(30) references User(username),
 	receiverName varchar(30) references User(username),
 	messageID integer references Message(messageID),
 	primary key(senderName, receiverName, messageID)
 );
 
-create table Listing(
-	serviceID integer,
-	listingID integer,
-	minPrice float,
-	maxPrice float,
-	startingTime time,
-	endingTime time,
-	startDate date,
-	endDate date,
-	latitudeLocation float,
-	longitudeLocation float,
-	listingDescription varchar(100),
-	availability varchar(20) check(availability='Everywhere' or availability='Restricted' or availability='WalkIn'),
-	availabilityDistance double,
-	primary key(serviceID, listingID)
-);
 
-create table Appointment(
-	buyerName varchar(30) references Buyer(username),
-	listingID integer references Listing(ListingID),
-	serviceID integer references Service(serviceID),
-	price float,
-	isConfirmed boolean,
-	primary key(buyerName, listingID, serviceID)
-);
-
-create table RequestedService(
-	serviceID integer primary key references Service(serviceID)
-);
-
-create table OfferedService(
-	serviceID integer primary key references Service(serviceID)
-);
-
-create table Requests(
-	buyerName varchar(30) references Buyer(username),
-	serviceID integer references RequestedService(serviceID),
-	primary key(serviceID,buyerName)
-	
-);
-
-create table Offers(
+create table offers(
 	vendorName varchar(30) references Vendor(username),
 	serviceID integer references OfferedService(serviceID),
 	primary key(serviceID, vendorName)
