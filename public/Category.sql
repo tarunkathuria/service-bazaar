82c1af118046f7b36296c9e63b044edba0587029
Rohan Prinja <rohan.prinja@gmail.com>
2013-11-12 00:05:07 +0530

    Added category model and migrated

--- public/Category.sql
+++ public/Category.sql
@@ -1,28 +1,14 @@
 use service_bazaar;
-
-delete from Category;
-
-insert into Category values(1, "Delivery", "Services related to delivery of various items like Tiffin Delivery, etc.");
-
-insert into Category values(2, "Hospitality", "Hotel and Motel Bookings, Sightseeing services, etc.");
-
-insert into Category values(3, "Household", "Services related to various household repairs and maintenance like Pest Control, Electricians, Painters etc.");
-
-insert into Category values(4, "Travel and Transportation", "These services include the transportation by the land, by the air and by the sea. Transportation by lands includes the services of public utility vehicles, taxi’s and buses. Transportation by sea includes the services offered by ships, ferries and several others. Transportation by air includes the services offered by several air lines");
-
-insert into Category values(5, "Beauty Care", "Services that offer services for beauty enhancement. These services are most likely offered by the cosmetics and surgery clinics, the barber shops, facial centers and the like");
-
-insert into Category values(6, "Utility", "Services that are needed inside the house. These services include the electricity, the water, garbage collections and several others.");
-
-insert into Category values(7, "Legal Services", "Services that are most likely provided by the lawyers of law firms. These services include the preparation of legal documents; such things like the power of attorney, contracts, the court litigations, legal consultations and more.");
-
-insert into Category values(8, "Food services", "Involves the services offered by fast food chains, restaurants and stores which offer foods or snacks.");
-
-insert into Category values(9, "Education", "Educational services offered in pre-schol, elementary, secondary and college levels like Schools, Private Tuitons etc.");
-
-insert into Category values(10, "Maid Services", "Services which include maid, cooks, baysitters, etc.");
-
-insert into Category values(11, "Medicinal and Healthcare", "These are the services that are offered by the hospitals, the clinics, nursing homes, maternity homes, health insurance companies and the like.");
-
-insert into Category values(12, "Others", "Everything else");
-
+delete from category;
+insert into category values(1, "Delivery", "Services related to delivery of various items like Tiffin Delivery, etc.");
+insert into category values(2, "Hospitality", "Hotel and Motel Bookings, Sightseeing services, etc.");
+insert into category values(3, "Household", "Services related to various household repairs and maintenance like Pest Control, Electricians, Painters etc.");
+insert into category values(4, "Travel and Transportation", "These services include the transportation by the land, by the air and by the sea. Transportation by lands includes the services of public utility vehicles, taxi’s and buses. Transportation by sea includes the services offered by ships, ferries and several others. Transportation by air includes the services offered by several air lines");
+insert into category values(5, "Beauty Care", "Services that offer services for beauty enhancement. These services are most likely offered by the cosmetics and surgery clinics, the barber shops, facial centers and the like");
+insert into category values(6, "Utility", "Services that are needed inside the house. These services include the electricity, the water, garbage collections and several others.");
+insert into category values(7, "Legal Services", "Services that are most likely provided by the lawyers of law firms. These services include the preparation of legal documents; such things like the power of attorney, contracts, the court litigations, legal consultations and more.");
+insert into category values(8, "Food services", "Involves the services offered by fast food chains, restaurants and stores which offer foods or snacks.");
+insert into category values(9, "Education", "Educational services offered in pre-schol, elementary, secondary and college levels like Schools, Private Tuitons etc.");
+insert into category values(10, "Maid Services", "Services which include maid, cooks, baysitters, etc.");
+insert into category values(11, "Medicinal and Healthcare", "These are the services that are offered by the hospitals, the clinics, nursing homes, maternity homes, health insurance companies and the like.");
+insert into category values(12, "Others", "Everything else");
